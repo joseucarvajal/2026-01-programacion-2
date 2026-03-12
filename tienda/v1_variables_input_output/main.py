9d27493000e26f7743fc06043534e126e043848a
joseucarvajal <joseucarvajal@gmail.com>
2026-03-11 22:17:01 -0500

    V1: Variables, input, output

---
+++ tienda/v1_variables_input_output/main.py
@@ -0,0 +1,22 @@
+'''
+Aplicación para tienda
+Version: v1
+
+Que incluye:
+    - Variables
+        - input
+        - output
+'''
+
+#output
+print("Ingrese el nombre del producto que va a comprar: ")
+
+#input
+nombre_producto = input()
+
+#output
+print(f"El producto que quiere comprar se llama: {nombre_producto}")
+
+
+
+
