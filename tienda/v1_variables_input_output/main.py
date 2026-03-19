69a62cb6e63468e00e347019d3d56b5f5ba7ee84
joseucarvajal <joseucarvajal@gmail.com>
2026-03-18 22:19:41 -0500

    Decisiones y ciclos

--- tienda/v1_variables_input_output/main.py
+++ tienda/v1_variables_input_output/main.py
@@ -6,16 +6,52 @@ Que incluye:
     - Variables
         - input
         - output
+        - Tipos de variables: 
+            - String (str): cadenas de texto o de caracteres. Ejemplo: "hola", "precio es 50", "10"
+            - Numéricas entero (int): 5, 100, 134, -1234
+            - Numéricas float (float): 70.5, 83.1235
+            - Boolean (bool): True o False
+            - Objeto: Tipos personalizados
+        - Operadores:
+            - + : suma
+            - - : resta
+            - * : multiplicación
+            - / : división
+
+Qué problemas tiene esta versión (v1): BUGS
+    1. No valida que el nombre del producto NO sea vacío: Se resuelve en la v2
+    2. No valida que el precio del producto NO sea vacío: Se resuelve en la v2
+    3. No se valida que el precio del producto sea numérico* (No se resuelve en la versión 2, sino en una versión que contenga "Manejo de errores")
+    4. No se valida que el descuento del producto sea numérico* (No se resuelve en la versión 2, sino en una versión que contenga "Manejo de errores")
+    5. No se valida que el precio del producto sea positivo: Se resuelve en la v2
+    6. Si el descuento es vacío, entonces no se debe aplicar descuento
+    7. No se valida que el descuento del producto sea positivo: Se resuelve en la v2
+    8. No se valida que el precio del producto sea menor o igual que el descuento del producto. Es decir, el descuento no puede superar el valor
+        del producto: Se resuelve en la v2
 '''
 
 #output
 print("Ingrese el nombre del producto que va a comprar: ")
-
-#input
-nombre_producto = input()
+#input + variable
+nombre_del_producto = input()
 
 #output
-print(f"El producto que quiere comprar se llama: {nombre_producto}")
+print("Ingrese el precio del producto que va a comprar: ")
+#input + variable
+precio_del_producto = input()
+precio_del_producto = float(precio_del_producto)
+
+#output
+print("Ingrese el valor del descuento del producto: ")
+#input + variable
+descuento = input()
+descuento = float(descuento)
+
+precio_final_del_producto = precio_del_producto - descuento
+
+#output
+print(f"El producto que quiere comprar se llama: {nombre_del_producto} y tiene un precio de: {precio_final_del_producto}")
+
 
 
 
---
+++ tienda/v2_decisiones_validaciones/main.py
@@ -0,0 +1,49 @@
+'''
+Aplicación para tienda
+Version: v2
+
+Que incluye:
+    - Decisiones, validaciones
+
+Qué problemas tiene esta versión (v1): BUGS
+    1. El trabajo se puede perder, cuando se ingresa mal un valor, el programa termina. El programa no debería terminar, sino que debería REPETIR hasta
+    que el valor mal ingresado se ingrese bien. CICLOS (while - for): Se resuelve en la v3
+'''
+
+#output
+print("Ingrese el nombre del producto que va a comprar: ")
+#input + variable
+nombre_del_producto = input()
+if nombre_del_producto == "": #Solución al BUG número 1
+    print("ERROR: El nombre del producto no puede ser vacio")
+else:
+    #output
+    print("Ingrese el precio del producto que va a comprar: ")
+    #input + variable
+    precio_del_producto = input()
+    if precio_del_producto == "": #Solución al BUG número 2
+        print("ERROR: El precio del producto no puede ser vacío")
+    else:
+        precio_del_producto = float(precio_del_producto)
+        if precio_del_producto < 0: #Solución al BUG número 5
+            print("ERROR: El precio del producto no puede ser negativo")
+        else:
+            #output
+            print("Ingrese el valor del descuento del producto: ")
+            #input + variable
+            descuento = input()
+            if descuento == "": #Solución al BUG número 6
+                descuento = "0"
+
+            descuento = float(descuento)
+
+            if descuento < 0: #Solucíon al BUG número 7
+                print("ERROR: El descuento no puede ser un valor negativo")
+            else:
+                if precio_del_producto < descuento: #Solución al BUG número 8
+                    print("ERROR: El descuento no puede superar el valor del producto")
+                else:
+                    precio_final_del_producto = precio_del_producto - descuento
+
+                    #output
+                    print(f"El producto que quiere comprar se llama: {nombre_del_producto} y tiene un precio de: {precio_final_del_producto}")
